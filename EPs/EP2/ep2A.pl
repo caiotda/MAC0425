841218ce0b50a654619f1367c5f0e7b5d74bb447
Caio Andrade <caio.andrade@geekie.com.br>
2020-04-16 10:33:31 -0300

    Possibly finishes Exercise 1. Still needs to look out for corner cases, like checking the length of the set or list

--- EPs/EP2/ep2A.pl
+++ EPs/EP2/ep2A.pl
@@ -2,18 +2,18 @@
 %%%%% Use quantos predicados auxiliares julgar necess�rio
 
 lista_para_conjunto(Xs, Cs) :- 
-    lista_para_conjunto_helper(Xs, [], Result),
+    lista_para_conjunto_helper(Xs, [], Cs).
     
 lista_para_conjunto_helper([], Cs, Cs).
 
-lista_para_conjunto_helper([Xs|Xt], H, _) :-
+lista_para_conjunto_helper([Xs|Xt], H, Cs) :-
     not(member(Xs, H)),
     append(H, [Xs], Res),
-    lista_para_conjunto_helper(Xt, Res, _).
+    lista_para_conjunto_helper(Xt, Res, Cs).
 
-lista_para_conjunto_helper([Xs|Xt], H, _) :-
+lista_para_conjunto_helper([Xs|Xt], H, Cs) :-
     member(Xs, H),
-    lista_para_conjunto_helper(Xt, H, _).
+    lista_para_conjunto_helper(Xt, H, Cs).
 
 
 %%%%%%%% Fim dos predicados adicionados
